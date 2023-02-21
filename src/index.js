2eac7abc69e7058c762c98122487ed080e716e50
Kevin DC <kevindelcastillo@ravn.co>
2023-02-21 14:08:57 -0500

    refactor: Refactor JS files into TS

--- src/index.js
+++
@@ -1,6 +0,0 @@
-const app = require('./server')
-
-const port = 5001
-app.listen(port, () => {
-    console.log(`Serving on http://localhost:${port}`)
-})
--- src/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-
-const app = express()
-
-app.get('/', (req, res) => {
-    res.status(200).json({message: 'hello'})
-})
-
-module.exports = app
