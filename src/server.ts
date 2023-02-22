dcb42762ae5f5f5edeaaeb0ce3b99f484d05fbfa
Kevin DC <kevindelcastillo@ravn.co>
2023-02-22 11:32:49 -0500

    feat: Add endpoints to sign in and sign up

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { router } from './router';
 import morgan from 'morgan';
 import { authMiddleware } from './modules/auth';
+import * as authHandlers from './handlers/auth';
 
 export const server = express();
 
@@ -9,4 +10,6 @@ server.use(morgan('dev'));
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
+server.post('/api/sign-up', authHandlers.signUp);
+server.post('/api/sign-in', authHandlers.signIn);
 server.use('/api', authMiddleware, router);
