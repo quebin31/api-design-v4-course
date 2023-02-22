9cf275834c0618bc919af4315f728aef6efb770a
Kevin DC <kevindelcastillo@ravn.co>
2023-02-22 10:31:51 -0500

    refactor: Configure server port using an environment variable

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import { server } from './server';
+import dotenv from 'dotenv';
 
-const port = 5001;
+dotenv.config();
+const port = parseInt(process.env.PORT ?? '5001');
 
 server.listen(port, () => {
     console.log(`Serving on http://localhost:${port}`);
