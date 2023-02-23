a62a46877e5045b37f74983f9b7ac04eccc3a1da
Kevin DC <kevindelcastillo@ravn.co>
2023-02-23 09:26:37 -0500

    fix: Wrap async handlers for auth

--- src/routes/auth.router.ts
+++ src/routes/auth.router.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import * as authController from '../controllers/auth.controller';
+import asyncHandler from 'express-async-handler';
 
 const router = Router();
 
-router.post('/auth/sign-up', authController.signUp);
-router.post('/auth/sign-in', authController.signIn);
+router.post('/auth/sign-up', asyncHandler(authController.signUp));
+router.post('/auth/sign-in', asyncHandler(authController.signIn));
 
 export default router;
