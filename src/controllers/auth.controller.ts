fed4358277a0dfa3c7193abde25383a48cdbe7bd
Kevin DC <kevindelcastillo@ravn.co>
2023-02-24 11:43:36 -0500

    refactor: Refactor auth.controller.ts to move business logic out of the handlers

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,26 +1,10 @@
 import { Request, Response } from 'express';
-import { createJwt, isPasswordValid } from '../modules/auth';
 import * as authService from '../services/auth.service';
 
 export async function signUp(req: Request, res: Response) {
-    const user = await authService.createUser(req.body);
-    const accessToken = createJwt(user);
-    res.json({ id: user.id, accessToken });
+    res.json(authService.register(req.body));
 }
 
 export async function signIn(req: Request, res: Response) {
-    const { username, password } = req.body;
-
-    const user = await authService.findByUsername(username);
-    if (!user) {
-        throw new Error('Invalid username or password');
-    }
-
-    const isValid = await isPasswordValid(password, user.password);
-    if (!isValid) {
-        throw new Error('Invalid username or password');
-    }
-
-    const accessToken = createJwt(user);
-    res.json({ id: user.id, accessToken });
+    res.json(authService.validate(req.body));
 }
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,11 +1,36 @@
 import database from '../database';
-import { hashPassword } from '../modules/auth';
+import { createJwt, hashPassword, isPasswordValid } from '../modules/auth';
+import { UnauthorizedError } from '../errors';
 
-export async function createUser({ username, password }: { username: string, password: string }) {
-    const hashedPassword = await hashPassword(password);
-    return database.user.create({ data: { username, password: hashedPassword } });
+async function createUser(data: { username: string, password: string }) {
+    return database.user.create({ data });
 }
 
-export async function findByUsername(username: string) {
+async function findByUsername(username: string) {
     return database.user.findUnique({ where: { username } });
+}
+
+type AuthPayload = {
+    username: string,
+    password: string,
+}
+
+export async function register({ username, password }: AuthPayload) {
+    const hashedPassword = await hashPassword(password);
+    const user = await createUser({ username, password: hashedPassword });
+    return { id: user.id, accessToken: createJwt({ id: user.id }) };
+}
+
+export async function validate({ username, password }: AuthPayload) {
+    const user = await findByUsername(username);
+    if (!user) {
+        throw new UnauthorizedError('Invalid username or password');
+    }
+
+    const isValid = await isPasswordValid(password, user.password);
+    if (!isValid) {
+        throw new UnauthorizedError('Invalid username or password');
+    }
+
+    return { id: user.id, accessToken: createJwt({ id: user.id }) };
 }
