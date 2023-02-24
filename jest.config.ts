905f6e524b85c8aa9baa08ae86948229e2fe756a
Kevin DC <kevindelcastillo@ravn.co>
2023-02-24 16:41:52 -0500

    chore: Add Jest for unit testing

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    verbose: true,
+    collectCoverageFrom: ['**/*.ts'],
+    coveragePathIgnorePatterns: ['**/__test__/*', '**/*.test.*'],
+    watchPlugins: [
+        require.resolve('jest-watch-typeahead/filename'),
+        require.resolve('jest-watch-typeahead/testname'),
+    ],
+};
+
+export default config;
