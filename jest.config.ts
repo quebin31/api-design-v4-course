ce71301ac486ed016c33bc039cf875b021be3d0c
Kevin DC <kevindelcastillo@ravn.co>
2023-02-27 11:08:05 -0500

    chore: Add clear mocks configuration into jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from '@jest/types';
 
 const config: Config.InitialOptions = {
+    clearMocks: true,
     preset: 'ts-jest',
     testEnvironment: 'node',
     verbose: true,
